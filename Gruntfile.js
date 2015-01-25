f0a30dc0859d0bcf9cf8efbb8abbb8df0cebd29e
Tom Panning <tom.panning@nextcentury.com>
2015-01-24 19:40:37 -0500

    Set up npm, bower, and grunt for basic slides

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+var pkgjson = require('./bower.json');
+
+var config = {
+    pkg: pkgjson,
+    app: 'src',
+    dist: 'dist'
+};
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        config: config,
+        pkg: config.pkg,
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'bower_components/reveal.js',
+                    src: 'css/reveal.css',
+                    dest: '<%= config.dist %>'
+                }, {
+                    expand: true,
+                    cwd: 'bower_components/reveal.js/lib',
+                    src: 'css/zenburn.css',
+                    dest: '<%= config.dist %>'
+                }, {
+                    expand: true,
+                    cwd: 'bower_components/reveal.js/',
+                    src: 'css/print/*.css',
+                    dest: '<%= config.dist %>'
+                }, {
+                    expand: true,
+                    cwd: 'bower_components/reveal.js/',
+                    src: 'lib/font/source-sans-pro/*',
+                    dest: '<%= config.dist %>'
+                }, {
+                    expand: true,
+                    cwd: 'bower_components/reveal.js/lib',
+                    src: 'js/*.js',
+                    dest: '<%= config.dist %>'
+                }, {
+                    expand: true,
+                    cwd: 'bower_components/reveal.js/plugin/zoom-js',
+                    src: 'zoom.js',
+                    dest: '<%= config.dist %>/js'
+                }, {
+                    expand: true,
+                    cwd: 'bower_components/reveal.js',
+                    src: 'css/theme/*.css',
+                    dest: '<%= config.dist %>'
+                }, {
+                    expand: true,
+                    cwd: 'bower_components/reveal.js',
+                    src: 'js/reveal.js',
+                    dest: '<%= config.dist %>'
+                }, {
+                    expand: true,
+                    cwd: 'src/',
+                    src: 'index.html',
+                    dest: '<%= config.dist %>'
+                }]
+            }
+        }/*, uglify: {
+        }*/
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', [
+        'copy'/*,
+        'uglify'*/
+    ]);
+}
